0aa4b63add859615815233960f4432598a35fa39
Jiří Brtník <jirka.brtnik@post.cz>
2021-08-31 22:32:39 +0200

    My C lisp project

---
+++ get_your_ass.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+	puts("Get your ass here!");
+	puts("nice one");
+	return 0;
+}
---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+	puts("Get your ass here!");
+	return 0;
+}
