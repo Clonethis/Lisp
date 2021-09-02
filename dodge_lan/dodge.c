2abfe960438ad2a46c16a64d02c998a6aac5968a
unknown <jirka.brtnik@post.cz>
2021-09-02 21:56:52 +0200

    new way of work

---
+++ dodge_lan/dodge.c
@@ -0,0 +1,20 @@
+#include "mpc.h"
+int main(int arga,char** argv){
+	/*Building parser adjectives to recognize descriptions */
+	mpc_parser_t* Adjective = mpc_or(4,
+	mpc_sym("wow"),mpc_sym("many"),
+	mpc_sym("such"),mpc_sym("wow")
+	);
+	/*Build mpc parser 'Noun' to recognize things*/
+	mpc_parser_t* Noun = mpc_or(5,
+	mpc_sym("lisp"),mpc_sym("language"),
+	mpc_sym("hello"),mpc_sym("build"),
+	mpc_sym("c")
+	);
+	mpc_parser_t* Phares = mpc_and(2,mpcf_strfold,
+	Adjective , Noun, free);
+	mpc_parser_t* Doge = mpc_many)mpcf_strfold, Phrase);
+	/* Do some parsing here*/
+	mpc_delete(Doge);
+	return 0;
+}
