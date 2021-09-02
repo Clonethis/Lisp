3974f73712be4d07613c405bbd64c389780b0a25
unknown <jirka.brtnik@post.cz>
2021-09-02 09:10:26 +0200

    ..

--- 4-interactiveprompt/interactive.c
+++ 4-interactiveprompt/interactive.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 static char input[2048];
-int main (int argv, char** argv){
+int main (int arga, char** argv){
 	puts ("Lispy Version 0.0.0.1");
 	puts ("Press Ctrl+c to Exit");
+	/*infinite loop*/
 	while(1){
+		
+		/*Output our prompt??*/
+		/*Also doesn't print newline char*/
+		/*needs a file to write to -> stdout (special case) takes care of it*/
 		fputs("lisp>", stdout);
+		
+		/*readline max 2048 chars*/
+		/*same manner as 'fputs' | 'fgets' needs to read from file -> 'stdin' takes care of it*/
+		/*'fgets' waits for user input*/
+		/*'input' is variable that is passed down to 'printf'*/
 		fgets(input,2048,stdin);
+		
+		/*look at documentation for more use*/
+		/*http://en.cppreference.com*/
 		printf("No you're a %s",input);
 	}
 	return 0;
--- loops/for.c
+++ loops/for.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc,char** argv){
-	for (int i =0; i<100;i++){
-		printf("Loop %d\n",i);
+	for (int i =0; i<=100;i++){
+		printf("Loop i=%d\n",i);
 	}
 }
